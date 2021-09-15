182a5e0597cf52ae034f8cc32a1aa85b94b89881
Kshitija Patil <patilkshitija2@gmail.com>
2021-09-15 16:22:43 +0530

    [kshitija]:given userid return invoice

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,19 +1,26 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import com.bridgelabz.cabinvoicegenerator.InvoiceSummery;
+import java.util.Scanner;
 
 public class CabInvoiceGenerator {
 	double minimumCost = 10;
 	int costPerTime = 1;
 	int minimumFare = 5;
+	HashMap<Integer, InvoiceSummery> hashmap = new HashMap<Integer, InvoiceSummery>();
+	CabInvoiceGenerator cabInvoiceGenerator;
 
 	public double calculatefare(double distance, int time) {
 
 		double totalfare = distance * minimumCost + time * minimumCost;
-		
-			return Math.max(totalfare, minimumFare);
-		
 
+		if (totalfare < minimumFare)
+		return totalfare;
+		return minimumFare;
 	}
 
 	public InvoiceSummery calculatefare(Ride[] rides) {
@@ -22,7 +29,22 @@ public class CabInvoiceGenerator {
 			totalfare += this.calculatefare(ride.distance, ride.time);
 		}
 
-		return new InvoiceSummery(rides.length,totalfare);
+		return new InvoiceSummery(rides.length, totalfare);
 
+	}
+
+	public InvoiceSummery listOfRides(Ride[] rides, int num) {
+		double totalfare = 0;
+		for (Ride ride : rides) {
+			totalfare += this.calculatefare(ride.distance, ride.time);
+		}
+
+		hashmap.put(num, new InvoiceSummery(rides.length, totalfare));
+		
+		return new InvoiceSummery(rides.length, totalfare);
+	}
+
+	public String invoicePerId(int i) {
+		return String.valueOf(hashmap.get(i));
 	}
 }
--- src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
@@ -6,6 +6,7 @@ import com.bridgelabz.cabinvoicegenerator.CabInvoiceGenerator;
 import com.bridgelabz.cabinvoicegenerator.InvoiceSummery;
 import com.bridgelabz.cabinvoicegenerator.Ride;
 
+
 import org.junit.Assert;
 import org.junit.Before;
 
@@ -48,4 +49,28 @@ public class CabInvoiceGeneratorTest {
 		
 		Assert.assertEquals(expectedInvoicesummery, summery);
 	}
+	@Test
+	
+	public void givenUserId_InvoiceGenerator_shouldReturnRides() {
+		
+		Ride[] rides1= {
+				new Ride(2.0,5),
+				new Ride(0.1,1),
+				new Ride(3.0,6)
+				};
+		Ride[] rides2= {
+				new Ride(4.0,5),
+				new Ride(2.0,1),
+				new Ride(5.0,6)
+				};
+		cabInvoiceGenerator.listOfRides(rides1,1);
+	
+		Assert.assertEquals("InvoiceSummary{noOfRides=3, totalFare=30.0, average=15.0}",cabInvoiceGenerator.invoicePerId(1));
+		cabInvoiceGenerator.listOfRides(rides2,2);
+		
+		Assert.assertEquals("InvoiceSummary{noOfRides=3, totalFare=40.0, average=185.0}",cabInvoiceGenerator.invoicePerId(2));
+	}
 }
+
+
+
